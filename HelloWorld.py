d90f06721390968f5b268c7a17f18530c310a726
keerthi-prog <63771872+keerthi-prog@users.noreply.github.com>
2022-02-24 23:25:16 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+class HelloWorld:
+    def __init__(self):
+        print("HelloWorld constructor")
+    def printHelloWorld(self):
+        print("Hello World in Jenkins")
+
+hw = HelloWorld()
+hw.printHelloWorld()
